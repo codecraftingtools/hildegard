874da65f7876016b739f85ed8621a5cdb22746ba
Jeff Webb <jeff.webb@codecraftsmen.org>
2020-02-11 05:34:54 -0600

    Removed __init__ definitions from Connection, Component and Hierarchic Component

--- pidgen/__init__.py
+++ pidgen/__init__.py
@@ -9,21 +9,15 @@ class Component(Entity):
         Attribute("title", str, default="untitled"),
         Attribute("ports", OrderedDict, alias="port"),
     )    
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
 
 class Connection:
     _attributes = (
         Attribute("from_port"),
         Attribute("to_port"),
     )
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
         
 class Hierarchic_Component(Component):
     _attributes = (
         Attribute("subcomponents", OrderedDict, alias="subcomponent"),
         Attribute("connections", list, alias="connection"),
     )    
-    def __init__(self, *args, **kw):
-        super().__init__(*args, **kw)
